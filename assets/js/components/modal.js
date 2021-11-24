f815298030b50cb15d9587d172d35ab305def773
Leliksey <57276673+Leliksey@users.noreply.github.com>
2021-11-24 15:06:19 +0200

    first commmit

--- assets/js/components/modal.js
+++ assets/js/components/modal.js
@@ -1,9 +1,31 @@
-//function card () {
-    //alert("ee")
-    //   let modal = document.createElement("div");
-    //   modal.classList.add("modal");
-    //   modal.innerHTML = "<a href='#'>" + this.name + " " + this.abv + "%" + "<img src='" + this.img +"' alt='beer'>" + "</a>";
-    //   for (var i = 0; i < dropdownItem.length; i++) {
-    //     dropdownItem[i].append(modal);
-    //   }
-   //}
+// function sortAbv() {
+//             var items = document.querySelectorAll(".dropdown-item");
+            
+//             var parent = document.querySelector(".dropdown-list-show");
+            
+//             var SortElements = new Object();
+//             items.forEach(function (item, indx) {
+//               var itemValue = parseInt(
+//                 item
+//                   .querySelector(".item__text")
+//                   .textContent
+//                   .replace(/\D/g,'')
+                  
+//               );
+//               SortElements[itemValue] = { element: item, index: indx };
+//             });
+//             var keys = Object.keys(SortElements);
+//             function compareNumeric(a, b) {
+//               a = parseInt(a);
+//               b = parseInt(b);
+//               if (a < b) return 1;
+//               if (a > b) return -1;
+//             }
+//             keys.sort(compareNumeric);
+//             keys.map(function (key, indx) {
+//                 parent.insertAdjacentElement(
+//                 "beforeend",
+//                 SortElements[key]["element"]
+//               );
+//             });
+//         }
--- assets/js/components/oneMoreScript.js
+++ assets/js/components/oneMoreScript.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   var dropdownListHide = document.querySelectorAll(".dropdown-list-hide");
+  var btn__container = document.querySelectorAll(".btn__container");
   var btn = document.querySelector(".btn-group");
   var dropdownMenuPizza = document.querySelector(".dropdown-menu-pizza");
   var dropdownMenuSteak = document.querySelector(".dropdown-menu-steak");
@@ -10,6 +11,9 @@ window.onload = function () {
   var main__foodPairing = document.querySelector(".main__food-pairing");
   var main__beerImg = document.querySelector(".main__beer-img");
   var main__tagline = document.querySelector(".main__tagline");
+  var sortByNamePizza = document.querySelector(".sortByNamePizza");
+  var sortByNameSteak = document.querySelector(".sortByNameSteak");
+  var sortByNameAll = document.querySelector(".sortByNameAll");
   var sortByAbvPizza = document.querySelector(".sortByAbvPizza");
   var sortByAbvSteak = document.querySelector(".sortByAbvSteak");
   var sortByAbvAll = document.querySelector(".sortByAbvAll");
@@ -18,6 +22,7 @@ window.onload = function () {
     e.preventDefault();
     if (e.target.classList.contains("beerForPizza")) {
       dropdownListHide[0].classList.toggle("dropdown-list-show");
+      btn__container[0].classList.toggle("btn__container__show");
       if (dropdownMenuPizza.hasChildNodes()) {
         return;
       }
@@ -57,37 +62,52 @@ window.onload = function () {
               main__tagline.innerHTML = tagline;
             });
           }
-          // sortByAbvPizza.addEventListener("click", () => {
-          //   var items = document.querySelectorAll(".dropdown-item-pizza");
-          //   var parent = document.querySelector(".dropdown-menu-pizza");
-          //   var SortElements = new Object();
-          //   items.forEach(function (item, indx) {
-          //     var itemValue = parseInt(
-          //       item
-          //         .querySelector(".item__text__pizza")
-          //         .textContent
-          //         .replace(/\D/g,'')
-          //     );
-          //     SortElements[itemValue] = { element: item, index: indx };
-          //   });
-          //   var keys = Object.keys(SortElements);
-          //   function compareNumeric(a, b) {
-          //     a = parseInt(a);
-          //     b = parseInt(b);
-          //     if (a < b) return 1;
-          //     if (a > b) return -1;
-          //   }
-          //   keys.sort(compareNumeric);
-          //   keys.map(function (key, indx) {
-          //     parent.insertAdjacentElement(
-          //       "beforeend",
-          //       SortElements[key]["element"]
-          //     );
-          //   });
-          // });
+          sortByNamePizza.addEventListener("click", sortByName);
+          var sortByAbvPizza = document.querySelector(".sortByAbvPizza");
+      var toggle = false;
+      
+      sortByAbvPizza.addEventListener('click', function(){
+        
+        var items = document.querySelectorAll(".dropdown-item-pizza");
+        var parent = document.querySelector(".dropdown-menu-pizza");
+      var SortElements = new Object();
+      items.forEach(function(item, indx){
+        var itemValue = parseInt(item.querySelector('.item__text__pizza').textContent.replace(/\D/g, ""));
+        SortElements[itemValue] = {'element': item, 'index': indx} ;
+      });
+      var keys = Object.keys(SortElements);
+      function compareNumeric1(a, b) {
+        a = parseInt(a);
+        b = parseInt(b);
+        if (a < b) return 1;
+        if (a > b) return -1;
+      }
+        function compareNumeric2(a, b) {
+        a = parseInt(a);
+        b = parseInt(b);
+        if (a > b) return 1;
+        if (a < b) return -1;
+      }
+      if(toggle === false){
+        keys.sort(compareNumeric1);
+        toggle = !toggle;
+      } else {
+        keys.sort(compareNumeric2);
+        toggle = !toggle;
+      }
+      
+      keys.map(function(key, indx){
+        parent.insertAdjacentElement('beforeend', SortElements[key]['element']);
+        SortElements[key]['index'] = indx;
+      });
+        return toggle;
+      }, toggle);
         });
-    } else if (e.target.classList.contains("beerForSteak")) {
+    }
+    
+     else if (e.target.classList.contains("beerForSteak")) {
       dropdownListHide[1].classList.toggle("dropdown-list-show");
+      btn__container[1].classList.toggle("btn__container__show");
       if (dropdownMenuSteak.hasChildNodes()) {
         return;
       }
@@ -126,37 +146,51 @@ window.onload = function () {
               main__tagline.innerHTML = tagline;
             });
           }
-          // sortByAbvSteak.addEventListener("click", () => {
-          //   var items = document.querySelectorAll(".dropdown-item-steak");
-          //   var parent = document.querySelector(".dropdown-menu-steak");
-          //   var SortElements = new Object();
-          //   items.forEach(function (item, indx) {
-          //     var itemValue = parseInt(
-          //       item
-          //         .querySelector(".item__text__steak")
-          //         .textContent
-          //         .replace(/\D/g,'')
-          //     );
-          //     SortElements[itemValue] = { element: item, index: indx };
-          //   });
-          //   var keys = Object.keys(SortElements);
-          //   function compareNumeric(a, b) {
-          //     a = parseInt(a);
-          //     b = parseInt(b);
-          //     if (a < b) return 1;
-          //     if (a > b) return -1;
-          //   }
-          //   keys.sort(compareNumeric);
-          //   keys.map(function (key, indx) {
-          //     parent.insertAdjacentElement(
-          //       "beforeend",
-          //       SortElements[key]["element"]
-          //     );
-          //   });
-          // });
+          sortByNameSteak.addEventListener("click", sortByName);
+          var sortByAbvSteak = document.querySelector(".sortByAbvSteak");
+      var toggle = false;
+      
+      sortByAbvSteak.addEventListener('click', function(){
+        
+        var items = document.querySelectorAll(".dropdown-item-steak");
+        var parent = document.querySelector(".dropdown-menu-steak");
+      var SortElements = new Object();
+      items.forEach(function(item, indx){
+        var itemValue = parseInt(item.querySelector('.item__text__steak').textContent.replace(/\D/g, ""));
+        SortElements[itemValue] = {'element': item, 'index': indx} ;
+      });
+      var keys = Object.keys(SortElements);
+      function compareNumeric1(a, b) {
+        a = parseInt(a);
+        b = parseInt(b);
+        if (a < b) return 1;
+        if (a > b) return -1;
+      }
+        function compareNumeric2(a, b) {
+        a = parseInt(a);
+        b = parseInt(b);
+        if (a > b) return 1;
+        if (a < b) return -1;
+      }
+      if(toggle === false){
+        keys.sort(compareNumeric1);
+        toggle = !toggle;
+      } else {
+        keys.sort(compareNumeric2);
+        toggle = !toggle;
+      }
+      
+      keys.map(function(key, indx){
+        parent.insertAdjacentElement('beforeend', SortElements[key]['element']);
+        SortElements[key]['index'] = indx;
+      });
+        return toggle;
+      }, toggle);
         });
-    } else if (e.target.classList.contains("beerAll")) {
+    }
+     else if (e.target.classList.contains("beerAll")) {
       dropdownListHide[2].classList.toggle("dropdown-list-show");
+      btn__container[2].classList.toggle("btn__container__show");
       if (dropdownMenuAll.hasChildNodes()) {
         return;
       }
@@ -195,39 +229,189 @@ window.onload = function () {
               main__tagline.innerHTML = tagline;
             });
           }
-          // sortByAbvAll.addEventListener("click", () => {
-          //   var items = document.querySelectorAll(".dropdown-item-all");
-            
-          //   var dropdownMenuAll = document.querySelector(".dropdown-menu-all");
-            
-          //   var SortElements = new Object();
-          //   items.forEach(function (item, indx) {
-          //     var itemValue = parseInt(
-          //       item
-          //         .querySelector(".item__text__all")
-          //         .textContent
-          //         .replace(/\D/g,'')
-                  
-          //     );
-          //     SortElements[itemValue] = { element: item, index: indx };
-          //   });
-          //   var keys = Object.keys(SortElements);
-          //   function compareNumeric(a, b) {
-          //     a = parseInt(a);
-          //     b = parseInt(b);
-          //     if (a < b) return 1;
-          //     if (a > b) return -1;
-          //   }
-          //   keys.sort(compareNumeric);
-          //   keys.map(function (key, indx) {
-          //     dropdownMenuAll.insertAdjacentElement(
-          //       "beforeend",
-          //       SortElements[key]["element"]
-          //     );
-          //   });
-          // });
+          sortByNameAll.addEventListener("click", sortByName);
+          var sortByAbvAll = document.querySelector(".sortByAbvAll");
+      var toggle = false;
+      
+      sortByAbvAll.addEventListener('click', function(){
+        var items = document.querySelectorAll(".dropdown-item-all");
+        var parent = document.querySelector(".dropdown-menu-all");
+      var SortElements = new Object();
+      items.forEach(function(item, indx){
+        var itemValue = parseInt(item.querySelector('.item__text__all').textContent.replace(/\D/g, ""));
+        SortElements[itemValue] = {'element': item, 'index': indx} ;
+      });
+      var keys = Object.keys(SortElements);
+      function compareNumeric1(a, b) {
+        a = parseInt(a);
+        b = parseInt(b);
+        if (a < b) return 1;
+        if (a > b) return -1;
+      }
+        function compareNumeric2(a, b) {
+        a = parseInt(a);
+        b = parseInt(b);
+        if (a > b) return 1;
+        if (a < b) return -1;
+      }
+      if(toggle === false){
+        keys.sort(compareNumeric1);
+        toggle = !toggle;
+      } else {
+        keys.sort(compareNumeric2);
+        toggle = !toggle;
+      }
+      
+      keys.map(function(key, indx){
+        parent.insertAdjacentElement('beforeend', SortElements[key]['element']);
+        SortElements[key]['index'] = indx;
+      });
+        return toggle;
+      }, toggle);
         });
     }
   }
   btn.addEventListener("click", getData);
+
+  function sortByName(e) {
+    if (e.target.classList.contains("sortByNamePizza")) {
+      var list,
+        i,
+        switching,
+        b,
+        shouldSwitch,
+        dir,
+        switchcount = 0;
+      list = document.querySelectorAll(".dropdown-menu-pizza");
+      switching = true;
+      dir = "asc";
+      while (switching) {
+        switching = false;
+        for (var i = 0; i < list.length; i++) {
+          b = list[i].querySelectorAll(".dropdown-item");
+          for (i = 0; i < b.length - 1; i++) {
+            shouldSwitch = false;
+            if (dir == "asc") {
+              if (
+                b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()
+              ) {
+                shouldSwitch = true;
+                break;
+              }
+            } else if (dir == "desc") {
+              if (
+                b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()
+              ) {
+                shouldSwitch = true;
+                break;
+              }
+            }
+          }
+          if (shouldSwitch) {
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+
+            switchcount++;
+          } else {
+            if (switchcount == 0 && dir == "asc") {
+              dir = "desc";
+              switching = true;
+            }
+          }
+        }
+      }
+    } else if (e.target.classList.contains("sortByNameSteak")) {
+      var list,
+        i,
+        switching,
+        b,
+        shouldSwitch,
+        dir,
+        switchcount = 0;
+      list = document.querySelectorAll(".dropdown-menu-steak");
+      switching = true;
+      dir = "asc";
+      while (switching) {
+        switching = false;
+        for (var i = 0; i < list.length; i++) {
+          b = list[i].querySelectorAll(".dropdown-item");
+          for (i = 0; i < b.length - 1; i++) {
+            shouldSwitch = false;
+            if (dir == "asc") {
+              if (
+                b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()
+              ) {
+                shouldSwitch = true;
+                break;
+              }
+            } else if (dir == "desc") {
+              if (
+                b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()
+              ) {
+                shouldSwitch = true;
+                break;
+              }
+            }
+          }
+          if (shouldSwitch) {
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+
+            switchcount++;
+          } else {
+            if (switchcount == 0 && dir == "asc") {
+              dir = "desc";
+              switching = true;
+            }
+          }
+        }
+      }
+    } else if (e.target.classList.contains("sortByNameAll")) {
+      var list,
+        i,
+        switching,
+        b,
+        shouldSwitch,
+        dir,
+        switchcount = 0;
+      list = document.querySelectorAll(".dropdown-menu-all");
+      switching = true;
+      dir = "asc";
+      while (switching) {
+        switching = false;
+        for (var i = 0; i < list.length; i++) {
+          b = list[i].querySelectorAll(".dropdown-item");
+          for (i = 0; i < b.length - 1; i++) {
+            shouldSwitch = false;
+            if (dir == "asc") {
+              if (
+                b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()
+              ) {
+                shouldSwitch = true;
+                break;
+              }
+            } else if (dir == "desc") {
+              if (
+                b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()
+              ) {
+                shouldSwitch = true;
+                break;
+              }
+            }
+          }
+          if (shouldSwitch) {
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+
+            switchcount++;
+          } else {
+            if (switchcount == 0 && dir == "asc") {
+              dir = "desc";
+              switching = true;
+            }
+          }
+        }
+      }
+    }
+  }
 };
